8b52efdf737ed518ef2edcc89f70f0e036c6a918
shisir <shisirhumagain0@gmail.com>
2025-05-08 18:35:43 -0500

    producer added

---
+++ src/main/java/com/shisir/activemq/MessageProducer.java
@@ -0,0 +1,17 @@
+package com.shisir.activemq;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageProducer
+{
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    public void sendMessage(String destination, String message){
+        jmsTemplate.convertAndSend(destination,message);
+        System.out.println("Sent message: " + message);
+    }
+}
